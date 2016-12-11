93269e7470121c03e6ebb54c9b3687ede1dc375d
guojie <yingsunsuikong@sina.com>
2016-12-11 15:33:12 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ApplicationRecord
   def is_member_of?(group)
     participated_groups.include?(group)
   end
+
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
